489ec5062298d275e48bd5595dc43eee5885338c
unknown <vemant81@gmail.com>
2024-02-26 02:03:45 +0300

    Main WebAuto 4_2 added new tests

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -34,3 +34,52 @@ describe("Github page tests", () => {
     expect(actual).toContain("Sign up for free", { timeout: 180000 });
   });
 });
+
+// Новые три теста написаны в данном блоке; тестируется страница https://github.blog
+describe("Github page tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.blog");
+  });
+
+  afterEach(() => {
+    page.close();
+  });
+
+  // Тест, проверяющий наличие надписи "Blog" вверху страницы
+  test("Checking for the presence of the 'Blog' inscription", async () => {
+    const blogSelector = await page.$(
+      "body > header > div > nav > div > div.d-flex.flex-items-center > a.d-inline-block.Header-link.font-weight-semibold.ml-2.f2.color-fg-default"
+    );
+    const actual = await page.$eval(blogSelector, (link) => link.textContent);
+    expect(actual).toEqual("Blog", { timeout: 180000 });
+  });
+
+  // Тест, проверяющий переход по вкладке "Engineer"
+  test("Going to the engineer tab", async () => {
+    const engineerTub = await page.$(
+      "body > header > div > nav > div > div.p-plus-container > div.p-plus.p-plus--is-showing-toggle.p-plus--is-hiding-primary.p-plus--is-showing-overflow > ul > li:nth-child(1) > a"
+    );
+    await engineerTub.click();
+
+    const engineerHead = "#start-of-content > div > h1";
+    await page.waitForSelector(engineerHead, {
+      visible: true,
+    });
+    const actual = await page.$eval(engineerHead, (link) => link.textContent);
+    expect(actual).toEqual("Engineering", { timeout: 180000 });
+  });
+
+  // Тест, проверяющий, что открылась нужная вкладка после нажатия кнопки "Try GitHub Copilot"
+  test("Creation a new tab", async () => {
+    const copilotButton = await page.$(
+      "body > header > div > nav > div > div.p-plus-container > div.p-plus.p-plus--is-showing-toggle.p-plus--is-hiding-primary.p-plus--is-showing-overflow > ul > li:nth-child(11) > a:nth-child(1)"
+    );
+    actualCopilotPage = await copilotButton.click();
+    expectedCopilotPage = await browser.newPage();
+    await expectedCopilotPage.goto(
+      "https://github.com/features/copilot?utm_source=blog&utm_medium=topnav&utm_campaign=cta&utm_content=copilot"
+    );
+    expect(expectedCopilotPage).toEqual(actualCopilotPage, { timeout: 180000 });
+  });
+});
