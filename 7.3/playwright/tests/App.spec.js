b8debc10b5eb0c48f3885b84259914d2bb9985cb
unknown <vemant81@gmail.com>
2024-02-28 17:51:58 +0300

    Main WebAuto 3_6 changed tests ../user.js

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,47 +1,42 @@
 const { test, expect } = require("@playwright/test");
 
-test("test", async ({ page }) => {
-  // Go to https://netology.ru/free/management#/
-  await page.goto("https://netology.ru/free/management#/");
+// test("test", async ({ page }) => {
+//   await page.goto("https://netology.ru/free/management#/");
 
-  // Click a
-  await page.click("a");
-  await expect(page).toHaveURL("https://netology.ru/");
+//   await page.click("a");
+//   await expect(page).toHaveURL("https://netology.ru/");
 
-  // Click text=Учиться бесплатно
-  await page.click("text=Учиться бесплатно");
-  await expect(page).toHaveURL("https://netology.ru/free");
+//   await page.click("text=Учиться бесплатно");
+//   await expect(page).toHaveURL("https://netology.ru/free");
 
-  page.click("text=Бизнес и управление");
+//   page.click("text=Бизнес и управление");
 
-  // Click text=Как перенести своё дело в онлайн
-  await page.click("text=Как перенести своё дело в онлайн");
-  await expect(page).toHaveURL(
-    "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-  );
-});
+//   await page.click("text=Как перенести своё дело в онлайн");
+//   await expect(page).toHaveURL(
+//     "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
+//   );
+// });
 
-test("test", async ({ page }) => {
-  const { username, password } = require("@playwright/user.js");
+test("right login and password", async ({ page }) => {
+  const { username, password } = require("../user.js");
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.getByPlaceholder("Email").click();
   await page.getByPlaceholder("Email").fill(username);
   await page.getByPlaceholder("Email").press("Tab");
   await page.getByPlaceholder("Пароль").fill(password);
   await page.getByTestId("login-submit-btn").click();
-  await expect(page.getByClass("styles_title__QUu_b")).toHaveText([
-    "Направления обучения",
-  ]);
+  await expect(page.getByText("Моё обучение")).toBeVisible();
 });
 
-test("test", async ({ page }) => {
+test("wrong login and password", async ({ page }) => {
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.getByPlaceholder("Email").click();
   await page.getByPlaceholder("Email").fill("jkldfndfn@mail.ru");
   await page.getByPlaceholder("Email").press("Tab");
   await page.getByPlaceholder("Пароль").fill("ldkbnlsdknbas");
   await page.getByTestId("login-submit-btn").click();
-  await expect(page.getByTestId("login-error-hint")).toHaveText([
-    "Вы ввели неправильно логин или пароль",
-  ]);
+  await expect(page.getByTestId("login-error-hint")).toHaveText(
+    ["Вы ввели неправильно логин или пароль"],
+    { timeout: 10000 }
+  );
 });
