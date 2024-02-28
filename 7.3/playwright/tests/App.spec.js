4db5160b37fc3bf1de46398c6d42f51d4bac4cff
unknown <vemant81@gmail.com>
2024-02-28 18:59:41 +0300

    Main WebAuto 3_8 deleted example test

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,22 +1,5 @@
 const { test, expect } = require("@playwright/test");
 
-// test("test", async ({ page }) => {
-//   await page.goto("https://netology.ru/free/management#/");
-
-//   await page.click("a");
-//   await expect(page).toHaveURL("https://netology.ru/");
-
-//   await page.click("text=Учиться бесплатно");
-//   await expect(page).toHaveURL("https://netology.ru/free");
-
-//   page.click("text=Бизнес и управление");
-
-//   await page.click("text=Как перенести своё дело в онлайн");
-//   await expect(page).toHaveURL(
-//     "https://netology.ru/programs/kak-perenesti-svoyo-delo-v-onlajn-bp"
-//   );
-// });
-
 test("right login and password", async ({ page }) => {
   const { username, password } = require("../user.js");
   await page.goto("https://netology.ru/?modal=sign_in");
@@ -25,7 +8,7 @@ test("right login and password", async ({ page }) => {
   await page.getByPlaceholder("Email").press("Tab");
   await page.getByPlaceholder("Пароль").fill(password);
   await page.getByTestId("login-submit-btn").click();
-  await expect(page.getByText("Моё обучение")).toBeVisible();
+  await expect(page.getByText("Моё обучение")).toBeVisible({ timeout: 20000 });
 });
 
 test("wrong login and password", async ({ page }) => {
